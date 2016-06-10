f37b8449db7451c5a87eb8328e77e9bffa45d14f
Denner, Stefan <denner.stefan@student.fhws.de>
2016-06-10 05:30:52 +0200

    .

---
+++ app/src/main/java/hack/fhws/de/artemplate/network.java
@@ -0,0 +1,17 @@
+package hack.fhws.de.artemplate;
+
+
+
+/**
+ * Created by stefan on 10.06.16.
+ */
+public class network {
+    public static long getRemainingTime(String studiengang, int semester, long uhrzeit) {
+        return 1465285400000L;
+    }
+    public static String getEmailAdress(String studiengang, int semester, long uhrzeit){
+        return "peter.braun@fhws.de";
+    }
+
+}
+
