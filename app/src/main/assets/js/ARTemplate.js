124e57ec6863c8094e3264f7872a8baf8f127c51
Steffen Kastner <kastner_s@freenet.de>
2016-06-10 14:06:43 +0200

    end

--- app/src/main/assets/js/ARTemplate.js
+++ app/src/main/assets/js/ARTemplate.js
@@ -9,25 +9,23 @@ var World = {
 
 	createOverlays: function createOverlaysFn() {
 
-		this.tracker = new AR.ClientTracker("assets/tracker.wtc", {	});
+		this.tracker = new AR.ClientTracker("assets/tracker.wtc", {});
 
         this.modelUhr = new AR.HtmlDrawable({html: "<div><h1>Noch 5 Minuten</h1></div>"}, 1, {
             viewportWidth: 128,
             viewportHeight: 128,
-            horizontalAnchor : AR.CONST.HORIZONTAL_ANCHOR.LEFT,
-            opacity : 0.9
+            horizontalAnchor: AR.CONST.HORIZONTAL_ANCHOR.LEFT
         });
 
-		var imageResource = new AR.ImageResource("assets/cat.jpg");
-		this.modelCat = new AR.ImageDrawable(imageResource, 5, {
-            offsetX: 1,
+		this.modelCat = new AR.ImageDrawable(new AR.ImageResource("assets/cat.jpg"), 1, {
             onClick: function() {
                 this.rotation += 10;
             }
         });
-        var bewertungsImage = new AR.ImageResource("assets/cat.jpg");
-        this.modelBewertung= new AR.ImageDrawable(bewertungsImage, 5, {
-            offsetX:  1,
+
+        this.modelBewertung= new AR.ImageDrawable(
+            new AR.ImageResource("assets/cat.jpg"),
+            1, {
             onClick: function() {
                 this.rotation += 10;
             }
@@ -37,58 +35,50 @@ var World = {
             viewportWidth: 1024,
             viewportHeight: 1024,
             scale: 2,
-          onClick : function() {
-            window.open("mailto:peter.braun@fhws.de");
-          },
-          horizontalAnchor : AR.CONST.HORIZONTAL_ANCHOR.LEFT,
-          opacity : 0.9
+            offsetX: -1,
+            onClick : function() {
+                window.open("mailto:peter.braun@fhws.de");
+            },
+            horizontalAnchor : AR.CONST.HORIZONTAL_ANCHOR.LEFT
         });
 
-        var imgAckermann = new AR.ImageResource("assets/Ackermann.png");
-        this.modelAcker = new AR.ImageDrawable(imgAckermann, 1, {
-            offsetX: 0
+        this.modelAcker = new AR.ImageDrawable(
+            new AR.ImageResource("assets/Ackermann.png"), 1, {offsetX: 0});
+
+        this.modelVorlesung = new AR.ImageDrawable(
+            new AR.ImageResource("assets/vorlesung2.png"), 1, {scale: 0.75, offsetX: 0});
+
+        this.trackableBewertung = new AR.Trackable2DObject(this.tracker, "MarkerBewertung", {
+            drawables: {
+                cam: [this.modelVorlesung]
+            }
         });
-        var imgVorlesung = new AR.ImageResource("assets/vorlesung2.png");
-        this.modelVorlesung = new AR.ImageDrawable(imgVorlesung, 1, {
-            scale: 0.75,
-            offsetX: 0
+
+		this.trackableDozent = new AR.Trackable2DObject(this.tracker, "MarkerDozent", {
+            drawables: {
+                cam: [this.modelBraun]
+            }
         });
-
-
-		/*
-			To receive a notification once the image target is inside the field of vision the onEnterFieldOfVision trigger of the AR.Trackable2DObject is used. In the example the function appear() is attached. Within the appear function the previously created AR.AnimationGroup is started by calling its start() function which plays the animation once.
-		*/
-		var trackableBewertung = new AR.Trackable2DObject(this.tracker, "MarkerBewertung", {
-                    drawables: {
-                        cam: [this.modelVorlesung]
-                    }
-                });
-
-		var trackableDozent = new AR.Trackable2DObject(this.tracker, "MarkerDozent", {
-        			drawables: {
-        				cam: [this.modelBraun]
-        			}
-        		});
-        var trackablePinnwand = new AR.Trackable2DObject(this.tracker, "MarkerPinnwand", {
-        			drawables: {
-        				cam: [this.modelPinnwand]
-        			}
-        		});
-        var trackablePostfach = new AR.Trackable2DObject(this.tracker, "MarkerPostfach", {
-        			drawables: {
-        				cam: [this.modelCat]
-        			}
-        		});
-        var trackableUhr = new AR.Trackable2DObject(this.tracker, "MarkerUhr", {
-        			drawables: {
-        				cam: [this.modelUhr]
-        			}
-        		});
-        var trackableVorlesung = new AR.Trackable2DObject(this.tracker, "MarkerVorlesung", {
-        			drawables: {
-        				cam: [this.modelAcker]
-        			}
-        		});
+        this.trackablePinnwand = new AR.Trackable2DObject(this.tracker, "MarkerPinnwand", {
+            drawables: {
+                cam: [this.modelPinnwand]
+            }
+        });
+        this.trackablePostfach = new AR.Trackable2DObject(this.tracker, "MarkerPostfach", {
+            drawables: {
+                cam: [this.modelCat]
+            }
+        });
+        this.trackableUhr = new AR.Trackable2DObject(this.tracker, "MarkerUhr", {
+            drawables: {
+                cam: [this.modelUhr]
+            }
+        });
+        this.trackableVorlesung = new AR.Trackable2DObject(this.tracker, "MarkerVorlesung", {
+            drawables: {
+                cam: [this.modelAcker]
+            }
+        });
 
 	}
 };
--- app/src/main/java/hack/fhws/de/artemplate/MainActivity.java
+++ app/src/main/java/hack/fhws/de/artemplate/MainActivity.java
@@ -1,14 +1,10 @@
 package hack.fhws.de.artemplate;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.wikitude.architect.ArchitectView;
 import com.wikitude.architect.StartupConfiguration;
